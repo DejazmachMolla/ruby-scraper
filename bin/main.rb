a803f62e3806e9a5783deee657eb7c1c1b2c8b03
Dejazmach Getachew <jilogical@gmail.com>
2020-08-16 20:11:31 +0300

    Scrapped the homepage

--- bin/main.rb
+++ bin/main.rb
@@ -1,1 +1,12 @@
 #!/usr/bin/env ruby
+require_relative '../lib/scrapper.rb'
+require_relative '../lib/job.rb'
+require_relative '../lib/about_job.rb'
+
+def scrape(target_url)
+  scrapper = Scrapper.new(target_url)
+  scrapper
+end
+
+scrapper = scrape('http://www.ethiojobs.net')
+puts scrapper.scrapped_page
