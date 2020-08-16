ede5ee679563f7b088deb1419db8a10d8f3aca8d
Dejazmach Getachew <jilogical@gmail.com>
2020-08-16 20:54:59 +0300

    Created a method to fill job detail

--- lib/job.rb
+++ lib/job.rb
@@ -7,4 +7,26 @@ class Job
     @job_title = job_title
     @job_url = job_url
   end
+
+  def create_job_detail
+    job = self
+    begin
+      scrapper = Scrapper.new(job.job_url)
+      detail_page = scrapper.scrapped_page
+      job_listing = detail_page.css('div#listingsResults')
+      unless job_listing.nil?
+        job.id_by_ethiojobs = job_listing.at('//div[4]').css('div.page-header/span.jobs_by/span.num').text
+        job.job_description = job_listing.css('div.listingInfo.col-md-12.marg-top-1')
+        job.logo_url = Job.create_logo_url(job_listing)
+        deadline_unformmated = job_listing.css('div.JobTaskMenu/div/span.post_deadline').first.text.to_s
+        job.deadline = deadline_unformmated[8, deadline_unformmated.length]
+        job.posted_on = find_posted_on(job_listing)
+        job.abouts = create_abouts(detail_page)
+      end
+      job
+    rescue URI::InvalidURIError
+      job
+    end
+  end
+  
 end
