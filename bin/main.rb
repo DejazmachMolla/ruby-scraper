84f4f90e7329518ad30c242bec21cf0c39c9c652
Dejazmach Getachew <jilogical@gmail.com>
2020-08-16 20:16:11 +0300

    Added a url generator method for different pages

--- bin/main.rb
+++ bin/main.rb
@@ -3,10 +3,19 @@ require_relative '../lib/scrapper.rb'
 require_relative '../lib/job.rb'
 require_relative '../lib/about_job.rb'
 
+def url_generator(page, category_name)
+  if page == 'home'
+    'http://www.ethiojobs.net'
+  else
+    "http://www.ethiojobs.net/browse-by-category/#{category_name.to_uri}/?action=search&
+    page=1&listings_per_page=100&view=list"
+  end
+end
+
 def scrape(target_url)
   scrapper = Scrapper.new(target_url)
   scrapper
 end
 
-scrapper = scrape('http://www.ethiojobs.net')
+scrapper = scrape(url_generator('home', nil))
 puts scrapper.scrapped_page
