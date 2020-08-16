96779448e3996ca78afeec90b5f6cfefb423ba72
Dejazmach Getachew <jilogical@gmail.com>
2020-08-16 20:37:33 +0300

    Added methods to display formatted category name

--- lib/display.rb
+++ lib/display.rb
@@ -1,8 +1,28 @@
 require 'colorize'
 
 module Display
+  def self.horizontal_line(sym, width)
+    sym * width
+  end
+
+  def self.left_indent(str)
+    left_indent = (130 - str.length) / 2
+    left_indent_str = ''
+    left_indent_str = ' ' * left_indent if left_indent.positive?
+    left_indent_str
+  end
+  
   def self.display_no_of_categories(num)
     info = "#{num} CATEGORIES FOUND"
     puts info.green
+  end
+
+  def self.display_category_name(category_name)
+    left_indent_str = left_indent(category_name)
+    puts "\n"
+    puts horizontal_line('*', 130).cyan
+    puts "#{left_indent_str}#{category_name.upcase} JOBS"
+    puts horizontal_line('*', 130).cyan
+    puts
   end
 end
